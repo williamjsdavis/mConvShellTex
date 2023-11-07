044a0c960bafd348a361db7f84bdbbc3c1b625c2
William Davis <38541020+williamjsdavis@users.noreply.github.com>
2023-11-07 11:48:49 -0800

    Add example temperature data

--- mconvshelltex/mconvshelltex.py
+++ mconvshelltex/mconvshelltex.py
@@ -7,6 +7,13 @@ def load_xygrid():
         d_out = pickle.load(input_file)
     return d_out
 
+"""Load example temperature data"""
+def load_example_tdata():
+    filename = "mConvShellTex/mconvshelltex/tdata-example.pkl"
+    with open(filename, "rb") as input_file:
+        d_out = pickle.load(input_file)
+    return d_out
+
 def hello():
     print("Hello")
     return 0
