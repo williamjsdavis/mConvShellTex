5c01cbdd902407a7e9dffefd4be1a0e46971b837
William Davis <38541020+williamjsdavis@users.noreply.github.com>
2023-11-07 11:42:27 -0800

    Redo file path

--- mconvshelltex/mconvshelltex.py
+++ mconvshelltex/mconvshelltex.py
@@ -3,7 +3,7 @@ import os
 
 """Load xy grid data"""
 def load_xygrid():
-    filename = "xygrid.pkl"
+    filename = "mConvShellTex/mconvshelltex/xygrid.pkl"
     print(os.popen('pwd').read())
     with open(filename, "r") as input_file:
         d_out = pickle.load(input_file)
