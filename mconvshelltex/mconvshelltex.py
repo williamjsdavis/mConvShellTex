0a12d8efb4ccd3c486fc861587ea31646184e940
William Davis <38541020+williamjsdavis@users.noreply.github.com>
2023-11-07 11:35:26 -0800

    Add xygrid data

--- mconvshelltex/mconvshelltex.py
+++ mconvshelltex/mconvshelltex.py
@@ -1,5 +1,13 @@
+import pickle
 
+"""Load xy grid data"""
+def load_xygrid():
+    filename = "xygrid.pkl"
+    with open(filename, "r") as input_file:
+        d_out = pickle.load(input_file)
+    return d_out
 
 def hello():
     print("Hello")
     return 0
+
